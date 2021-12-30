ef33fe3d4afab347daf68f7c4666844887d202f1
Raquel Ribeiro <raquelg.ribeiro26@gmail.com>
2021-12-30 16:49:45 -0300

    Criando Aplicativo Android de Jogar Dados

---
+++ app/src/main/java/com/example/a04_diceroller/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.a04_diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+
+            val toast = Toast.makeText(this,"A sorte foi lançada!", Toast.LENGTH_SHORT).show()
+
+            val resultTextView: TextView = findViewById(R.id.textView)
+            rollDice()
+
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int){
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
